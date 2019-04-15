605659959ad55e7292ecb0dc0e84aeb899672c8a
Joshua Downer <joshua.downer@gmail.com>
2019-04-15 09:53:18 +0000

    tests: added test_matrix.py

---
+++ tests/test_matrix.py
@@ -0,0 +1,90 @@
+from rigidbody import Matrix
+
+def test_matrix_equality():
+
+    A = Matrix(3, 3)
+    B = Matrix(3, 3)
+
+    assert(A == B)
+
+    A = Matrix.identity(3, 3)
+    B = Matrix.identity(3, 3)
+
+    assert(A == B)
+
+    A = Matrix.zero(3, 3)
+    B = Matrix.zero(3, 3)
+
+    assert(A == B)
+
+
+def test_matrix_identities():
+
+    A = Matrix.identity(3, 3)
+    A[0, 1] = 1
+
+    assert(A + Matrix.zero(3, 3) == A)
+    assert(Matrix.zero(3, 3) + A == A)
+
+    assert(A * Matrix.identity(3, 3) == A)
+    assert(Matrix.identity(3, 3) * A == A)
+
+
+def test_matrix_addition():
+
+    A = Matrix(3, 3)
+    B = Matrix(3, 3)
+    C = Matrix(3, 3)
+
+    A[0, 1] = 1
+    B[1, 0] = 2
+
+    C[0, 1] = 1
+    C[1, 0] = 2
+
+    assert(A + B == C)
+    assert(B + A == C)
+
+
+def test_matrix_subtraction():
+
+    A = Matrix(3, 3)
+    B = Matrix(3, 3)
+    C = Matrix(3, 3)
+
+    A[0, 1] = 1
+    B[1, 0] = 2
+
+    C[0, 1] = 1
+    C[1, 0] = 2
+
+    assert(C - A == B)
+    assert(C - B == A)
+
+
+def test_matrix_multiplication():
+
+    A = Matrix(3, 3)
+    B = Matrix(3, 3)
+    AB = Matrix(3, 3)
+    BA = Matrix(3, 3)
+
+    A[0, :] = [1, 0, 0]
+    A[1, :] = [0, 0, 1]
+    A[2, :] = [0, 1, 0]
+
+    B[0, :] = [1, 2, 3]
+    B[1, :] = [4, 5, 6]
+    B[2, :] = [7, 8 ,9]
+
+    AB[0, :] = [1, 2, 3]
+    AB[1, :] = [7, 8 ,9]
+    AB[2, :] = [4, 5, 6]
+
+    assert(A * B == AB)
+
+    BA[0, :] = [1, 3, 2]
+    BA[1, :] = [4, 6, 5]
+    BA[2, :] = [7 ,9, 8]
+
+    assert(B * A == BA)
